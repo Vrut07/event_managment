d27f2e992b2174bb3621f4f0492b7cfecea21ce3
Vrut07 <vrutttalsaniya@gmail.com>
2023-08-20 12:53:19 +0530

    commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>EVENT MANAGEMENT LOGIN FORM </title>
+<link  rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<form  action="1.php" method="post" class="form">
+<h2>LOGIN</h2>
+<br>
+<input type="text" name="username"  class="box" placeholder="ENTER USERNAME"/>
+<br>
+<br>
+<input type="password" name="password"  class=" box" placeholder="ENTER PASSWORD" />
+<br>
+<input type="submit"  id="submit" value="submit">
+</form>
+</body>
+</html>
